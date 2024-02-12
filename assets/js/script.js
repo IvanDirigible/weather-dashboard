33599d3908a94c8aaa7b325b75608e9b6e0232f0
IvanDirigible <sickeruncible@gmail.com>
2024-02-12 17:47:30 -0500

    Added boilerplate styling. Layout fixed. Working on search button functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
-var MyKey = "8efc6ed3835ac107525a71beb81bc896";
-var city;
-var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${MyKey}`;
+var searchFormEl = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+  console.log(event);
+  console.log(searchInputVal);
+  console.log(document.querySelector('#search-input').value);
+  console.log(queryString);
+  var searchInputVal = document.querySelector('#search-input').value;
+
+  if (!searchInputVal) {
+    console.error('Please enter a city name.');
+    return;
+  }
+
+  var queryString = `http://api.openweathermap.org/data/2.5/weather?q=${searchInputVal}&appid=${MyKey}`;
+
+  location.assign(queryString);
+
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
