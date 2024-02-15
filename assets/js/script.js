ef8682c0d5e1b9de79eb5dccbe33825329c4d712
IvanDirigible <sickeruncible@gmail.com>
2024-02-15 15:14:25 -0500

    All cards for 5-day rendering! Next is past searches.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ var nowWindEl = document.getElementById("now-wind")
 var nowHumidEl = document.getElementById("now-humid")
 // var inputFormEl = $('#search-input');
 var inputFormEl = document.getElementById('search-input');
+var fiveContainerEl = document.getElementById("five-container");
 // var date = new Date(unixDate * 1000);
 var icon ="";
 // var iconLink = `http://openweathermap.org/img/w/${icon}.png`;
@@ -25,14 +26,15 @@ var getHandler = function (city) {
     fetch(queryURL).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
+                console.log(data)
                 var tempNow = data.main.temp
                 var windNow = data.wind.speed
                 var humidNow = data.main.humidity
                 var dateNow = (data.dt)
                 var date = new Date(dateNow *1000)
                 var renderDate = date.toDateString();
+                console.log(`Date: ${date}`)
                 icon = data.weather[0].icon
-                var iconNow = document.getElementById("icon-now")
                 var iconLink = `http://openweathermap.org/img/w/${icon}.png`;
                 nowHeaderEl.textContent = `${city} (${renderDate})`
                 var iconNow = new Image(50, 50);
@@ -43,21 +45,63 @@ var getHandler = function (city) {
                 nowHumidEl.append(` ${humidNow} %`)
                 lat = data.coord.lat
                 lon = data.coord.lon
-                var query5DayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${MyKey}`
+                var query5DayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${MyKey}&units=imperial`
                 console.log(`Lat:${lat}, Lon:${lon}`)
                 fetch(query5DayURL).then(function(response) {
                     response.json().then(function(renderData) {
-                        return console.log(renderData)
+                        console.log(renderData);
+                        for (var i = 6; i < 40; i += 8) {
+                            var temp = renderData.list[i].main.temp
+                            var wind = renderData.list[i].wind.speed
+                            var humid = renderData.list[i].main.humidity
+                            var dateFive = renderData.list[i].dt
+                            var date = new Date(dateFive *1000)
+                            var renderDate = date.toDateString();
+                            var icon = renderData.list[i].weather[0].icon
+                            var iconLink = `http://openweathermap.org/img/w/${icon}.png`;
+                            var iconNew = new Image(50, 50);
+                            iconNew.src = iconLink
+
+                            var fiveCard = document.createElement('div');
+                            fiveCard.classList.add('card', 'bg-warning');
+
+                            var fiveDate = document.createElement('h5')
+                            fiveDate.textContent = renderDate
+
+                            // var fiveIcon = document.createElement('p')
+                            // fiveIcon.innerHTML = iconNew
+
+                            var fiveTemp = document.createElement('p')
+                            fiveTemp.innerHTML = `Temp: ${temp} °F`
+
+                            var fiveWind = document.createElement('p')
+                            fiveWind.innerHTML = `Wind: ${wind} MPH`
+                            
+                            var fiveHumid = document.createElement('p')
+                            fiveHumid.innerHTML = `Humidity: ${humid} %`
+                            
+                            fiveCard.append(fiveDate, iconNew, fiveTemp, fiveWind, fiveHumid)
+                            // fiveContainerEl.appendChild(fiveIcon)
+                            fiveContainerEl.append(fiveCard);
+                        }
+                        
                     })
                 })
             });
         } else {
             return console.error("Sorry, there was an issue with your request.")
         }
-    }).then(function (data) {
-        for (var i = 0; i < 40; i += 8) {
-            var fiveDayTemp = data.lst[i].main.temp
-        }
+    }).then(function () {
+        console.log("Render searched button to list here.")
+        // for (var i = 0; i < 40; i += 8) {
+        //     renderFive(data.results[i]);
+            
+
+        //     fiveHeaderEl.appendChild(iconFive)
+        //     fiveTempEl.append(` ${tempFive} °F`)
+        //     fiveWindEl.append(` ${windFive} MPH`)
+        //     fiveHumidEl.append(` ${humidFive} %`)
+        // }
     })
 };
 
@@ -72,7 +116,41 @@ function submitHandler(event) {
     getHandler(inputFormEl)
 }
 
-// function printWeather
+// function renderFive(data) {
+//     console.log(data);
+//     var temp = data.list[i].main.temp
+//     var wind = data.list[i].main.wind.speed
+//     var humid = data.list[i].main.humidity
+//     var dateFive = data.list[i].main.dt
+//     var date = new Date(dateFive *1000)
+//     var renderDate = date.toDateString();
+//     icon = data.weather[i].icon
+//     var iconLink = `http://openweathermap.org/img/w/${icon}.png`;
+//     fiveHeaderEl.textContent = `${city} (${renderDate})`
+//     var iconFive = new Image(50, 50);
+//     iconFive.src = iconLink
+
+//     var fiveCard = document.createElement('div');
+//     fiveCard.classList.add('card', 'bg-warning');
+
+//     var fiveDate = document.createElement('h5')
+//     fiveDate.textContent = renderDate
+
+//     var fiveIcon = document.createElement('p')
+//     fiveIcon.innerHTML = icon
+
+//     var fiveTemp = document.createElement('p')
+//     fiveTemp.innerHTML = temp
+
+//     var fiveWind = document.createElement('p')
+//     fiveWind.innerHTML = wind
+    
+//     var fiveHumid = document.createElement('p')
+//     fiveHumid.innerHTML = humid
+
+//     fiveCard.append(fiveDate, fiveIcon, fiveTemp, fiveWind, fiveHumid)
+//     fiveContainerEl.append(fiveCard);
+// }
 
 // function getHandler(inputFormEl){
 //     city = inputFormEl.value;
