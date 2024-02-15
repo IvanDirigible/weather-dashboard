10a559ff93dca24c05fe5a62c58859311d8e3bc9
IvanDirigible <sickeruncible@gmail.com>
2024-02-15 11:43:30 -0500

    Request working now. Need to get info rendering next.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,38 @@
 var MyKey = "8efc6ed3835ac107525a71beb81bc896";
-var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${MyKey}`;
 var searchFormEl = document.getElementById('search-form');
 // var inputFormEl = $('#search-input');
 var inputFormEl = document.getElementById('search-input');
 var city = "";
 var lat = "";
 var lon = "";
-var state = "";
-var country = "";
+// var state = "";
+// var country = "";
 var limit = 1;
-var query5DayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${MyKey}`
 var submitEl = document.getElementById("submit-btn");
-var geocodingDURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=${limit}&appid=${MyKey}`;
+// var geocodingDURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=${limit}&appid=${MyKey}`;
 
-
+var getHandler = function (city) {
+    city = inputFormEl.value
+    var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${MyKey}`;
+    
+    fetch(queryURL).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                lat = data.coord.lat
+                lon = data.coord.lon
+                var query5DayURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${MyKey}`
+                console.log(`Lat:${lat}, Lon:${lon}`)
+                fetch(query5DayURL).then(function(response) {
+                    response.json().then(function(renderData) {
+                        return console.log(renderData)
+                    })
+                })
+            });
+        } else {
+            return console.error("Sorry, there was an issue with your request.")
+        }
+    }).then()
+};
 
 function submitHandler(event) {
   event.preventDefault();
@@ -26,27 +45,33 @@ function submitHandler(event) {
     getHandler(inputFormEl)
 }
 
-function getHandler(inputFormEl){
-    city = inputFormEl.value;
-    fetch(queryURL, {
-        method: "GET"
-    })
-    .then(function(response){
-        // console.log(response.status)
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-        lat = data.coord.lat;
-        lon = data.coord.lon;
-    })
-    fetch(query5DayURL, {
-        method: "GET"
-    })
-    .then(function (render5Day){
-        console.log("So far so good!")
-    })
-}
+// function getHandler(inputFormEl){
+//     city = inputFormEl.value;
+//     console.log(city)
+//     fetch(queryURL, {
+//         method: "GET"
+//     })
+//     console.log(`Success? ${queryURL}`)
+//     .then(function(city){
+//         console.log(city.status)
+//         console.log(city)
+//         lat = city.coord.lat
+//         lon = city.coord.lon
+//         console.log(lat)
+//         console.log(lon)
+//         return city.json()
+//     })
+//     .then(function(data){
+//         console.log(data);
+//         lat = data.coord.lat;
+//         lon = data.coord.lon;
+//     })
+//     fetch(query5DayURL, {
+//         method: "GET"
+//     })
+//     .then(function (render5Day){
+//         console.log("So far so good!")
+//     })
+// }
 
 submitEl.addEventListener("click", submitHandler)
-// searchFormEl.on('submit', submitHandler);
